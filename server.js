ad7cd7b26be2986a7c9a3359b7de80f12c26df45
Otis Singfield <singfieldotis@gmail.com>
2021-05-23 03:22:07 -0400

    added helpers and tests to shorten and optimize code

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const routes = require('./controllers');
 
 const path = require('path');
 const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
 
 const session = require('express-session');
 
@@ -24,7 +25,7 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
